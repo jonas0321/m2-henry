6e6ce1c0516abcd8aa816a5d2f74d466d847e2cf
jonas0321 <jonasdavid2009@hotmail.com>
2022-11-04 17:13:43 -0500

    modificaciones

--- 09-React-Routing/homework/01 - Exercises/src/App.js
+++ 09-React-Routing/homework/01 - Exercises/src/App.js
@@ -5,8 +5,21 @@ import NavBar from "./components/NavBar/NavBar.jsx";
 import Shipping from "./components/Shipping/Shipping.jsx";
 import Promotions from "./components/Promotions/Promotions.jsx";
 import CardDetail from "./components/CardDetail/CardDetail.jsx";
+import { Routes } from "react-router-dom";
+import { Route } from "react-router-dom";
 /* eslint-disable */
 
 export default function App() {
-  return <div></div>;
+  return (
+    <div>
+      <NavBar />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/shipping" element={<Shipping />}></Route>
+        <Route path="/promotions" element={<Promotions />}></Route>
+        <Route path="/cruises/:id" element={<CardDetail />}></Route>
+      </Routes>
+    </div>
+  );
 }
--- 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ 10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
-import './Contact.modules.css'
-
+import React from "react";
+import "./Contact.modules.css";
+import { useState } from "react";
 // eslint-disable-next-line
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
-export default function Contact () {
-  return <div>Crear Formulario</div>
+export default function Contact() {
+  const [inputs, setInputs] = React.useState({
+    name: "",
+    email: "",
+    phone: 0,
+    subject: "",
+    message: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) =>
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+
+  return (
+    <div>
+      Crear Formulario
+      <form>
+        <label>Nombre:</label>
+        <input
+          name="name"
+          placeholder="Escribe tu nombre..."
+          type="text"
+          value={inputs.name}
+          onChange={handleChange}
+        />
+        <label>Correo Electrónico:</label>
+        <input
+          name="email"
+          placeholder="Escribe tu email..."
+          type="text"
+          value={inputs.email}
+          onChange={handleChange}
+        />
+        <label>Teléfono:</label>
+        <input
+          name="phone"
+          placeholder="Escribe un teléfono..."
+          type="number"
+          value={inputs.phone}
+          onChange={handleChange}
+        />
+
+        <label>Asunto:</label>
+        <input
+          name="subject"
+          placeholder="Escribe el asunto..."
+          type="text"
+          value={inputs.subject}
+          onChange={handleChange}
+        />
+        <label>Mensaje:</label>
+        <textarea
+          name="message"
+          placeholder="Escribe tu mensaje..."
+          type="text"
+          value={inputs.message}
+          onChange={handleChange}
+        ></textarea>
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 }
